c25e6155f10ad9bf5d0507f9fcd46cba5e4af99e
Briones <j.alejandro.briones.a22@gmail.com>
2024-02-21 05:46:33 -0600

    fix: create new product

--- src/main/java/com/briones/productstoreapi/controllers/ProductsController.java
+++ src/main/java/com/briones/productstoreapi/controllers/ProductsController.java
@@ -1,25 +1,28 @@
 package com.briones.productstoreapi.controllers;
 
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Date;
 import java.util.List;
-
-import javax.naming.Binding;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.briones.productstoreapi.models.Product;
 import com.briones.productstoreapi.services.ProductsRepository;
 
 import jakarta.validation.Valid;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-
 
 @Controller
 @RequestMapping("/products")
@@ -50,7 +53,37 @@ public class ProductsController {
         if(result.hasErrors()) {
             return "products/CreateProduct";
         }
+
+        MultipartFile image = productDto.getImageFile();
+        Date createdAt = new Date();
+        String storageFileName = createdAt.getTime() + "_" + image.getOriginalFilename();
         
+        try {
+            String uploadDir = "public/img/";
+            Path uploadPath = (Path) Paths.get(uploadDir);
+
+            if(!Files.exists(uploadPath)) {
+                Files.createDirectories(uploadPath);
+            }
+
+            try (InputStream inputStream = image.getInputStream()) {
+                Files.copy(inputStream, Paths.get(uploadDir + storageFileName), StandardCopyOption.REPLACE_EXISTING);
+            }
+        } catch (Exception e) {
+            System.out.println("Exception: " + e.getMessage());
+        }
+
+        Product product = new Product();
+        product.setName(productDto.getName());
+        product.setBrand(productDto.getBrand());
+        product.setCategory(productDto.getCategory());
+        product.setPrice(productDto.getPrice());
+        product.setDescription(productDto.getDescription());
+        product.setCreatedAt(createdAt);
+        product.setImageFileName(storageFileName);
+
+        repo.save(product);
+
         return "redirect:/products";
     }
 }
