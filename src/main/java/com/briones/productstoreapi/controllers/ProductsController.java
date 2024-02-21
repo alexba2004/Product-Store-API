8d1338dc3901fcc3a1d60417f56a98e5679c0493
Briones <j.alejandro.briones.a22@gmail.com>
2024-02-21 05:23:22 -0600

    add/fix: create controller and fix createProduct.html

--- src/main/java/com/briones/productstoreapi/controllers/ProductsController.java
+++ src/main/java/com/briones/productstoreapi/controllers/ProductsController.java
@@ -2,14 +2,23 @@ package com.briones.productstoreapi.controllers;
 
 import java.util.List;
 
+import javax.naming.Binding;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.briones.productstoreapi.models.Product;
 import com.briones.productstoreapi.services.ProductsRepository;
+
+import jakarta.validation.Valid;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
@@ -30,5 +39,18 @@ public class ProductsController {
         ProductDto productDto = new ProductDto();
         model.addAttribute("productDto", productDto);
         return "products/createProduct";
+    }
+
+    @PostMapping({"/create"})
+    public String createProduct (@Valid @ModelAttribute ProductDto productDto, BindingResult result) {
+        if(productDto.getImageFile().isEmpty()) {
+            result.addError(new FieldError("productDto", "imageFile", "The image file is required"));
+        }
+
+        if(result.hasErrors()) {
+            return "products/CreateProduct";
+        }
+        
+        return "redirect:/products";
     }
 }
