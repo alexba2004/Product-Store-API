bdd794806e2ad3bde82f077f0d9bc8169b3359b6
Briones <j.alejandro.briones.a22@gmail.com>
2024-02-21 01:30:04 -0600

    add: Product-Store-API project

---
+++ src/main/java/com/briones/productstoreapi/ProductStoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.briones.productstoreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductStoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductStoreApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/briones/productstoreapi/ProductStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.briones.productstoreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductStoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
